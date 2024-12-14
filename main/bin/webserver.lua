b455bc2388047b905d3eeb3d9a1a2b872ad18386
DanXvoIsMe <157852596+DanXvoIsMe@users.noreply.github.com>
2024-12-14 14:17:39 +0100

    Update webserver.lua

--- main/bin/webserver.lua
+++ main/bin/webserver.lua
@@ -1,89 +1,140 @@
-local rn = require("racoonnet")
-local sysutils = require("sysutils")
 local component = require("component")
-local io = require("io")
-local filesystem = require("filesystem")
-local thread = require("thread")
 local event = require("event")
-local card, err = rn.init(sysutils.readconfig("racoonnet"))
-local config = {}
-config.directory = "/www/"
-local clientip, request, path
+local rn = require("racoonnet")
+local computer = require('computer')
+local sysutils = require("sysutils")
+local thread = require("thread")
 
-file_types = {}
-file_types["html"] = "text/html"
-local codes = {[302] = "Found", [400] = "Bad Request", [404] = "Not Found", [500] = "Internal Server Error"}
+local clients = {}
+local clientscard = {}
+local wan
+local lan = {}
+local ip
+local err
+local config = sysutils.readconfig("router")
+local lang = sysutils.readlang("router")
 
-if not card then
-  sysutils.log("Ошибка подключения к сети: \""..err.."\"!", 4, "webserver")
+
+--//Функция отправки пакета по ip получателя
+function route(recieverip, senderip, ... )
+  local m
+  local cl
+  for client in pairs(clients) do
+    m = recieverip:find(client)
+    if m then cl = client break end
+  end
+  if m then
+    lan[clientscard[cl]:sub(1,3)]:directsend(clients[cl], recieverip, senderip, ...)
+  else
+    if wan then
+    wan:directsend(wan.router, recieverip, senderip, ...)
+    end
+  end
+end
+
+--//Список команд роутера
+commands={}
+
+--//Пинг
+function commands.ping()
+  sysutils.log(lang.ping..": "..sendIP, 1, "router")
+  route(sendIP, recIP, "pong" )
+  return 
+end
+
+--//Версия
+function commands.ver()
+  sysutils.log(lang.ver..": "..sendIP, 1, "router")
+  route(sendIP, recIP, "DNSROUTER" )
+  return 
+end
+
+-- Define the local IP to DNS mapping
+local IPtoDNS = {
+    ["IP"] = "domen"
+}
+
+function commands.getip()
+    local adr
+    local acceptedPrefix = acceptedAdr:sub(1, 3)
+    local found = false
+    if lan[acceptedPrefix] then
+        local senderPrefix = senderAdr:sub(1, 3)
+        if IPtoDNS[senderPrefix] then
+            adr = IPtoDNS[senderPrefix]
+            found = true
+        else
+            found = false
+        end
+        if not found then
+            adr = ip .. "." .. senderPrefix
+        end
+        clients[adr] = senderAdr
+        clientscard[adr] = acceptedAdr
+        lan[acceptedPrefix]:directsend(senderAdr, adr, ip, "setip")
+        sysutils.log(lang.givenip .. ": " .. adr, 1, "router")
+    else
+        sysutils.log(lang.noLan .. ": " .. acceptedAdr, 2, "router")  -- Log if no LAN found
+    end
+end
+
+sysutils.log(lang.launch, 1, "router")
+if not config.lan then
+  sysutils.log(lang.noconfig, 4, "router")
   return
 end
-function senderror(code)
-  local codestr = code.." "..codes[code]
-  local html = "<html><body>"..codestr.."</body></html>"
-  local str = "HTTP/1.1 "..codestr.."\nContent-Type: text/html\nContent-Length:"..html:len().."\n\n"..html
-  card:send(clientip, str)
+
+
+--//Инициализируем WAN карту
+if config.wan.type then
+  wan, err = rn.init(config.wan)
+  if wan then
+    sysutils.log(lang.waninit..": \""..wan.address:sub(1,3).."\". ".."\". "..lang.gateway..": \""..wan.routerip.."\".", 0, "router")
+  else
+    sysutils.log(lang.wanerr..": \""..err.."\"!", 3, "router")
+  end
+else
+  sysutils.log(lang.nowan, 2, "router")
 end
 
-function redirect(redirto)
-  local resp = "HTTP/1.1 302 Found\nLocation: "..redirto.."\n\n";
-  card:send(clientip, resp)
+if wan then
+  ip = wan.ip
+else
+  ip = computer.address():sub(1,3)
 end
+sysutils.log("IP: \""..ip.."\"", 1, "router")
 
-function response()
-  clientip, request = card:receive()
-  if request:sub(1,3) == "GET" then
-    sysutils.log("Получен запрос. IP: \""..clientip.."\".", 1, "webserver")
-    path = request:match("GET .* HTTP/"):sub(5,request:match("GET .* HTTP/"):len()-6):gsub("[\n ]","")
-    if path == nil then senderror(400) return end
-    if path:match("%.%.") then senderror(400) return end
-    local fpath = filesystem.concat(config.directory, path)
-    if filesystem.exists(fpath) == false then senderror(404) return end
-    if filesystem.isDirectory(fpath) then 
-	  if path:sub(-1) ~= "/" then redirect(filesystem.concat(card.ip, path).."/") return end
-      if filesystem.exists(filesystem.concat(fpath, "index.html")) then
-	    redirect(card.ip..filesystem.concat(path, "index.html"))
-		return
-	  else
-	    local fcontent = "<html><body>Индекс \""..path.."\":<br><a href=\"../\">../</a><br>"
-		for name in filesystem.list(fpath)do
-		  fcontent = fcontent.."<a href=\"./"..name.."\">"..name.."</a><br>"
-		end
-		fcontent = fcontent.."</body></html>"
-	    local resp = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: "..fcontent:len().."\n\n"..fcontent;
-        card:send(clientip, resp)
-		return
-	  end
-	else
-	  if path:sub(-1) == "/" then redirect(filesystem.concat(card.ip, path)) return end
-	end
-    local file = io.open(fpath, "r")
-    local fcontent = file:read("*a")
-	if file_types[path:match("%.([%a%d]*)")] ~= nil then
-      ftype = file_types[path:match("%.([%a%d]*)")]
-    else
-      ftype = "text/plain"
-    end
-    local resp = "HTTP/1.1 200 OK\nContent-Type: "..ftype.."\nContent-Length: "..fcontent:len().."\n\n"..fcontent;
-    card:send(clientip, resp)
+--//Инициализируе LAN карты
+for saddr, obj in pairs(config.lan) do
+  obj.master = ip
+  lan[obj.address:sub(1,3)], err = rn.init(obj)
+  if lan[obj.address:sub(1,3)] then
+    sysutils.log(lang.laninit..": \""..lan[obj.address:sub(1,3)].address:sub(1,3).."\".", 0, "router")
+  else 
+    sysutils.log(lang.lanerr..": \""..err.."\"!", 3, "router")
   end
 end
 
-sysutils.log("Запущен WEB сервер. IP: \""..card.ip.."\".", 0, "webserver")
-
-function server()
+function routing()
   while true do
-    response()
+    packet, acceptedAdr, senderAdr, recIP, sendIP, command = rn.receiveall()
+  if recIP == ip or recIP == "" then
+    if commands[command] then
+        commands[command](table.unpack(packet,9))
+    end  
+  else
+    route(recIP,sendIP,table.unpack(packet,8))
+  end
   end
 end
 
-local t = thread.create(server)
+local t = thread.create(routing)
 
 while true do
   ev = {event.pull(_, "key_down")}
   local key=ev[4]
   if key==16 then --Q
     t:kill()
-	break
+  break
   end
 end
